0ca57aa2da6b1d5213ffb88398ac19abcb4f8e7b
schmidt95 <rts9cs@virginia.edu>
2014-09-26 16:02:57 -0400

    Skeletons added
    Added a few skeleton methods for the Player class.

--- ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/player/Player.java
+++ ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/player/Player.java
@@ -1,13 +1,49 @@
 package edu.virginia.cs.sgd.libgdx.player;
 
+import edu.virginia.cs.sgd.libgdx.inventory.Inventory;
+
 public class Player {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public int health;
+	private int defense;
+	public int power;
+	public int exp;
+	public int level;
+	
+	public Inventory inv;
+	
+	public Object weapon;
+	public Object armor;
+	
+//	/**
+//	 * @param args
+//	 */
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+	
+	public Player() {
+		health = 100;
+		defense = 100;
+		power = 100;
+		exp = 0;
+		level = 0;
 	}
+	
+	public int getAttPow() {
+		return power;
+		//return power + weapon.getPow();
+	}
+	
+	public void attacked(int attPow) {
+		health = health - attPow/defense;
+	}
+	
+	public void move() {
+		
+	}
+	
 
 }
