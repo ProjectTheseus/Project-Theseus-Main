820d33f4fa4560284c928c2f20fd9773818e58b6
dylanhellems <dylanhellems@gmail.com>
2014-09-26 15:28:45 -0400

    Added class files
    For player, entities, and inventory

---
+++ ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/entities/Entity.java
@@ -0,0 +1,13 @@
+package edu.virginia.cs.sgd.libgdx.entities;
+
+public class Entity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/player/Player.java
@@ -0,0 +1,13 @@
+package edu.virginia.cs.sgd.libgdx.player;
+
+public class Player {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
