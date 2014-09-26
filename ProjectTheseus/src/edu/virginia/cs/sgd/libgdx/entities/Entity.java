67180a3002626ba2058a8034e578b806368784c5
dlb3un <dlb3un@virginia.edu>
2014-09-26 15:45:43 -0400

    Methods
    added method stubs for entity

--- ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/entities/Entity.java
+++ ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/entities/Entity.java
@@ -1,10 +1,31 @@
 package edu.virginia.cs.sgd.libgdx.entities;
 
+//superclass for creatures
+//stats: health, defense, other things, location (point class)
+//skeleton methods: move, attack, find path
+
 public class Entity {
 
 	/**
 	 * @param args
 	 */
+	
+	public void move() {
+		
+	}
+	
+	public int attack() { //return number of points on attack
+		return 0;
+	}
+	
+	public void findPath() {
+		
+	}
+	
+	public void die() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
