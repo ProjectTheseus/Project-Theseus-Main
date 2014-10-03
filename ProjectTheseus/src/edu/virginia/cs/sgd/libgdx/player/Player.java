edd7e47d3d17dc943015ff39964540b7fa226728
rmccampbell <rmccampbell7@gmail.com>
2014-10-03 15:46:24 -0400

    Player constructor

--- ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/player/Player.java
+++ ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/player/Player.java
@@ -2,29 +2,35 @@ package edu.virginia.cs.sgd.libgdx.player;
 
 import edu.virginia.cs.sgd.libgdx.entities.Entity;
 
-public class Player extends Entity{
+public class Player extends Entity {
 
 	private int xp;
 	private int level;
-//	private QuickInventory quickInventory;
-//	private Inventory inventory;
+
+	// private QuickInventory quickInventory;
+	// private Inventory inventory;
+
+	public Player() {
+		this.xp = 0;
+		this.level = 1;
+	}
 
 	public void waitTurn() {
-		
+
 	}
-	
+
 	public void interact() {
-		
+
 	}
-	
+
 	public void accessInventory() {
-		
+
 	}
-	
+
 	public void level() {
 		level++;
 	}
-	
+
 	public void gainXp(int xp) {
 		this.xp += xp;
 	}
