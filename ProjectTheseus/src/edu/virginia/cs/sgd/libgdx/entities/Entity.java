da729d734f48911722350eccef82ca34340c52b9
dylanhellems <dylanhellems@gmail.com>
2014-10-09 18:52:05 -0400

    Merges done

--- ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/entities/Entity.java
+++ ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/entities/Entity.java
@@ -1,9 +1,7 @@
 package edu.virginia.cs.sgd.libgdx.entities;
 
-<<<<<<< HEAD
 import edu.virginia.cs.sgd.libgdx.inventory.Inventory;
 import edu.virginia.cs.sgd.libgdx.player.Player;
-=======
 import edu.virginia.cs.sgd.libgdx.g3d.MazeNode;
 
 //superclass for creatures
@@ -130,52 +128,5 @@ public class Entity {
 //	public void setArmor(Armor armor) {
 //		this.armor = armor;
 //	}
-
-	public static void main(String[] args) {
->>>>>>> origin/Combat
-
-public class Entity {
 	
-	private int maxHealth;
-	private int currentHealth;
-	private int defense;
-	private int viewDistance;
-	private int speed;
-	private int attStr;
-	
-	private int xp;
-	
-	public void move() {
-		
-	}
-	
-	public void detectPlayer() {
-		
-	}
-	
-	public void attack(Player p) {
-		p.takeDamage(attStr);
-	}
-	
-	public void takeDamage(int dmg) {
-		dmg = dmg - defense;
-		if (dmg >= 0) 
-			currentHealth -= dmg;
-		
-		if (currentHealth <= 0)
-			die();
-	}
-	
-	public void heal(int hp) {
-		currentHealth += hp;
-	}
-	
-	public void die() {
-		
-	}
-	
-	public void defend() {
-		
-	}
-
 }
