cbe270f826830a65e746444ae33116b4139fdde5
rmccampbell <rmccampbell7@gmail.com>
2014-10-17 15:40:09 -0400

    creature

--- ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/entities/Creature.java
+++ ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/entities/Creature.java
@@ -4,22 +4,17 @@ import edu.virginia.cs.sgd.libgdx.player.Player;
 
 public class Creature extends Entity {
 	private Player player;
-	
-	public Creature(){
+
+	public Creature() {
 		super();
 	}
-	
-	public void detectPlayer()
-	{
-		
-	}
-	
-	public void determineBestAction() {
-		//call the best method
-	}
-	
-	public boolean canAttackPlayer() {
+
+	public boolean detectPlayer() {
 		return false;
+	}
+
+	public void determineBestAction() {
+		// call the best method
 	}
 
 }
