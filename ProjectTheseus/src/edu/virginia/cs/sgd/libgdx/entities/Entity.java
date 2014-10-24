4c37e4023a66eeefe398e4d4d31c9f12568a6295
rmccampbell <rmccampbell7@gmail.com>
2014-10-24 15:12:55 -0400

    divided blocked damage by two

--- ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/entities/Entity.java
+++ ProjectTheseus/src/edu/virginia/cs/sgd/libgdx/entities/Entity.java
@@ -51,7 +51,7 @@ public class Entity {
 
 	public void takeDamage(int damage) {
 
-		int blocked = this.getTotalDefense();
+		int blocked = this.getTotalDefense() / 2;
 		int finalDamage = Math.min(damage - blocked, 0);
 		this.currentHealth -= finalDamage;
 		if (this.currentHealth <= 0) {
